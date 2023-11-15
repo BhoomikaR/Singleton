8263c6698a6e87b3c1b425b11fb258a4865db10a
Bhoomika Ramani <bhoomika.a.ramani@gmail.com>
2023-11-15 13:33:43 -0500

    Singleton : final singleton can be extended by extension

--- Singleton/ApiClient.swift
+++ Singleton/ApiClient.swift
@@ -8,10 +8,13 @@
 import UIKit
 
 //singleton class - ensure must init single isntance and has only one access point
-class ApiClient {
+final class ApiClient {
     static let instance = ApiClient()
     private init() {}
 }
-
+//final can't be subclass , so to add more behavior in swift use extension
+extension ApiClient{
+    
+}
 let client = ApiClient.instance
 
