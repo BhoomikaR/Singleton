383bb317d06cd47299b7fa2a5f3decd36b1b0fb6
Bhoomika Ramani <bhoomika.a.ramani@gmail.com>
2023-11-15 15:30:55 -0500

    Singleton : Used closure of APIClient in View controller

--- Singleton/ApiClient.swift
+++ Singleton/ApiClient.swift
@@ -7,14 +7,28 @@
 
 import UIKit
 
+struct LoggedInUser {}
+
 //singleton class - ensure must init single isntance and has only one access point
 final class ApiClient {
     static let instance = ApiClient()
     private init() {}
-}
-//final can't be subclass , so to add more behavior in swift use extension
-extension ApiClient{
     
+    func login(completion: (LoggedInUser) -> Void) {}
 }
+//final can't be subclass, so to add more behavior in swift use extension
+/*extension ApiClient{
+    
+}*/
+//singleton - with lower 's' convience constuctor - only get
+/*URLSession.shared*/
+
 let client = ApiClient.instance
 
+class LoginViewController: UIViewController {
+    func didTapLoginButton() {
+        ApiClient.instance.login() { user in
+            // show next screen
+        }
+    }
+}
