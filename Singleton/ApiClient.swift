8a23f36e16acb5f790aed98ce7584be83c9f2b6b
Bhoomika Ramani <bhoomika.a.ramani@gmail.com>
2023-11-15 17:06:34 -0500

    Singleton: level-4 module design - by inject closure function

--- Singleton/ApiClient.swift
+++ Singleton/ApiClient.swift
@@ -7,6 +7,16 @@
 
 import UIKit
 
+//Main Module
+
+extension ApiClient{
+    func login(completion: (LoggedInUser) -> Void) {}
+}
+
+extension ApiClient{
+    func loadFeed(completion: ([FeedItem]) -> Void) {}
+}
+
 //singleton class - ensure must init single isntance and has only one access point
 //API Module
 class ApiClient {
@@ -19,15 +29,14 @@ class ApiClient {
 //Login Module
 struct LoggedInUser {}
 
-extension ApiClient{
-    func login(completion: (LoggedInUser) -> Void) {}
-}
+
 class LoginViewController: UIViewController {
-    //properties injection & global state mutable state
-    var api = ApiClient.shared
+    
+    var login: (((LoggedInUser) -> Void)-> Void)?
     
     func didTapLoginButton() {
-        api.login() { user in
+        //inject closure
+        login? { user in
             // show next screen
         }
     }
@@ -36,28 +45,15 @@ class LoginViewController: UIViewController {
 //Feed Module
 struct FeedItem {}
 
-extension ApiClient{
-    func loadFeed(completion: ([FeedItem]) -> Void) {}
-}
 class FeedViewController: UIViewController{
-    var api = ApiClient.shared
+    var loadFeed: ((([FeedItem]) -> Void)-> Void)?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        api.loadFeed{ loadedItems in
+        loadFeed? { loadedItems in
             // update UI
             
         }
     }
 }
 
-
-//final can't be subclass, so to add more behavior in swift use extension
-/*extension ApiClient{
-    
-}*/
-
-//singleton - with lower 's' convience constuctor - only get
-/*URLSession.shared*/
-//unit test
-//class MockApiClient: ApiClient{}
