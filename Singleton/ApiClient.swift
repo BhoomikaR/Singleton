2bd1e62032d423f905a0b8345642eb5c10e11e66
Bhoomika Ramani <bhoomika.a.ramani@gmail.com>
2023-11-15 16:58:05 -0500

    Singleton: Level-2 Modular design - Separation of features from APIclient

--- Singleton/ApiClient.swift
+++ Singleton/ApiClient.swift
@@ -7,32 +7,24 @@
 
 import UIKit
 
+//singleton class - ensure must init single isntance and has only one access point
+//API Module
+class ApiClient {
+    static let shared = ApiClient()
+    private init() {}
+    func execute(_: URLRequest, completion:(Data) -> Void) {}
+}
+
+
+//Login Module
 struct LoggedInUser {}
 
-//singleton class - ensure must init single isntance and has only one access point
-class ApiClient {
-    static let instance = ApiClient()
-    private init() {}
-    
+extension ApiClient{
     func login(completion: (LoggedInUser) -> Void) {}
 }
-//final can't be subclass, so to add more behavior in swift use extension
-/*extension ApiClient{
-    
-}*/
-//singleton - with lower 's' convience constuctor - only get
-/*URLSession.shared*/
-
-//unit test
-class MockApiClient: ApiClient{
-    
-}
-
-let client = ApiClient.instance
-
 class LoginViewController: UIViewController {
-    //properties injection
-    var api = ApiClient.instance
+    //properties injection & global state mutable state
+    var api = ApiClient.shared
     
     func didTapLoginButton() {
         api.login() { user in
@@ -40,3 +32,32 @@ class LoginViewController: UIViewController {
         }
     }
 }
+
+//Feed Module
+struct FeedItem {}
+
+extension ApiClient{
+    func loadFeed(completion: ([FeedItem]) -> Void) {}
+}
+class FeedViewController: UIViewController{
+    var api = ApiClient.shared
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        api.loadFeed{ loadedItems in
+            // update UI
+            
+        }
+    }
+}
+
+
+//final can't be subclass, so to add more behavior in swift use extension
+/*extension ApiClient{
+    
+}*/
+
+//singleton - with lower 's' convience constuctor - only get
+/*URLSession.shared*/
+//unit test
+//class MockApiClient: ApiClient{}
